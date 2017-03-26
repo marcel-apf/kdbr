b1da16f89901a7d8f825617c6dd14ed7b0e959c5
Marcel Apfelbaum <marcel@redhat.com>
2017-03-26 16:34:41 +0300

    test: bugfixes to work with latest kdbr version

--- kdbr_test.c
+++ kdbr_test.c
@@ -9,11 +9,11 @@
 #include <errno.h>
 #include "kdbr.h"
 
-static int ioctl_register_device(int kdbr_fd, struct kdbr_reg *reg)
+static int ioctl_register_port(int kdbr_fd, struct kdbr_reg *reg)
 {
     int ret;
 
-    printf("kdbr register device\n");
+    printf("kdbr register port\n");
     ret = ioctl(kdbr_fd, KDBR_REGISTER_PORT, reg);
     if (ret == -1) {
         fprintf(stderr, "KDBR_REGISTER_DEVICE failed: %s\n", strerror(ret));
@@ -71,25 +71,29 @@ static int ioctl_close_conn(int port_fd, int conn_id)
 
 int main(int argc, char **argv)
 {
-    int kdbr_fd, port_fd, port, err, opt, r_id, conn_id, sender = 0;
+    int kdbr_fd, port_fd, port, err, conn_id, opt, sender = 0;
     char kdbr_port_name[80] = {0};
     struct kdbr_req sreq;
     struct kdbr_connection conn = {0};
     struct kdbr_reg reg = {0};
     char *buf = malloc(20);
-    char *buf1 = malloc(10);
-    char *buf2 = malloc(10);
 
-    while ((opt = getopt (argc, argv, "sr:g:")) != -1) {
+    while ((opt = getopt (argc, argv, "sg:p:q:r:")) != -1) {
         switch (opt) {
-        case 's':
+        case 's':  /* sender */
             sender = 1;
             break;
-        case 'r':
-            r_id = atoi(optarg);
-            break;
-	case 'g':
+	case 'g':  /* gid */
 	    reg.gid.id = atoi(optarg);
+	    break;
+	case 'p': /* remote gid */
+	    conn.peer.rgid.id = atoi(optarg);
+	    break;
+	case 'q':   /* queue id */
+            conn.queue_id = atoi(optarg);
+	    break;
+	case 'r':  /* remote queue id */
+	    conn.peer.rqueue = atoi(optarg);
 	    break;
         default:
             exit(1);
@@ -103,14 +107,15 @@ int main(int argc, char **argv)
     }
 
     printf("kdbr fd opened\n");
-    port = ioctl_register_device(kdbr_fd, &reg);
+    port = ioctl_register_port(kdbr_fd, &reg);
     if (port <= 0) {
         err = port;
         printf("Can't open device file: %s\n", KDBR_FILE_NAME);
         goto fail_kdbr_fd;
     }
 
-    printf("Opening port %d, pid %d\n", port, getpid());
+    printf("Opening port %d, gid %d, peer gid %d, queue %d, remote queue %d\n",
+	    port, reg.gid.id, conn.peer.rgid.id, conn.queue_id, conn.peer.rqueue);
 
     sprintf(kdbr_port_name, KDBR_FILE_NAME "%d", port);
     port_fd = open(kdbr_port_name, O_RDWR);
@@ -133,16 +138,10 @@ int main(int argc, char **argv)
     } else {
         struct kdbr_req rreq;
 
-        /*
-        rreq.vec[0].iov_base = buf2;
-        rreq.vec[0].iov_len = 10;
-        rreq.vec[1].iov_base = buf1;
-        rreq.vec[1].iov_len = 10;
-        rreq.vlen = 2;
-        */
         rreq.vec[0].iov_base = buf;
         rreq.vec[0].iov_len = 20;
         rreq.vlen = 1;
+	rreq.connection_id = conn_id;
 
 	rreq.flags = KDBR_REQ_SIGNATURE | KDBR_REQ_POST_RECV;
 	err = write(port_fd, &rreq, sizeof(rreq));
@@ -152,11 +151,12 @@ int main(int argc, char **argv)
 	}
     }
 
-    printf("Ready - Press Any Key to Continue\n");
+    printf(" Connection %d Ready - Press Any Key to Continue\n", conn_id);
     getchar();
 
     if (sender) {
 	sreq.flags = KDBR_REQ_SIGNATURE | KDBR_REQ_POST_SEND;
+	sreq.connection_id = conn_id;
 	err = write(port_fd, &sreq, sizeof(sreq));
 	if (err < 0) {
 		printf("write: err=%d, errno=%d\n", err, errno);
@@ -166,8 +166,6 @@ int main(int argc, char **argv)
         getchar();
     } else {
         printf("Message received buf='%s'\n", buf);
-        printf("Message received buf1='%s'\n", buf1);
-        printf("Message received buf2='%s'\n", buf2);
     }
 
     ioctl_close_conn(port_fd, conn_id);
@@ -192,8 +190,6 @@ fail_kdbr_fd:
     printf("kdbr fd closed\n");
 
 out:
-    free(buf2);
-    free(buf1);
     free(buf);
     return err;
 
